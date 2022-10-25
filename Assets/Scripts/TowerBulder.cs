bc06c2900111aa9a690cdcbf1ca0ff96a3463a42
Данил <wanty3@mail.ru>
2022-10-25 16:45:32 +0300

    Собрал платформу и спаунер

---
+++ Assets/Scripts/TowerBulder.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBulder : MonoBehaviour
+{
+    [SerializeField] private float _towerSize;
+    [SerializeField] private Transform _buildPoint;
+    [SerializeField] private Block _block;
+
+    private List<Block> _blocks;
+
+    private void Start()
+    {
+        Build();
+    }
+    public List<Block> Build()
+    {
+
+        _blocks = new List<Block>();
+
+        Transform currentPoint = _buildPoint;
+
+        for (int i = 0; i < _towerSize; i++)
+        {
+            Block newBlock = BuildBlock(currentPoint);
+            _blocks.Add(newBlock);
+            currentPoint = newBlock.transform;
+        }
+
+        return _blocks;
+    }
+    private Block BuildBlock(Transform currentBouildPoint)
+    {
+       return Instantiate(_block, GetBuildPoint(currentBouildPoint), Quaternion.identity, _buildPoint);
+    }  
+    
+    private Vector3 GetBuildPoint(Transform currentSegment)
+    {
+        return new Vector3(_buildPoint.position.x, currentSegment.position.y  + currentSegment.localScale.y + _block.transform.localScale.y/2, _buildPoint.position.z);
+    }
+}
